d6d4ac95d632cf8939640e7fb4210eea8cf3612b
James Ward <james@jamesward.com>
2020-12-14 13:01:05 -0700

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,19 +13,19 @@ java {
 }
 
 repositories {
-	mavenLocal()
 	mavenCentral()
 }
 
 dependencies {
+	implementation(kotlin("reflect"))
+	implementation(kotlin("stdlib-jdk8"))
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
+
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
