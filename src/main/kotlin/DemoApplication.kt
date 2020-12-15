40f6203307c60790b7e6a0ef6d174174c83bcd9b
James Ward <james@jamesward.com>
2020-12-15 10:56:15 -0700

    still no worky

--- src/main/kotlin/DemoApplication.kt
+++ src/main/kotlin/DemoApplication.kt
@@ -1,7 +1,6 @@
 package demo
 
 import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.fu.kofu.reactiveWebApplication
 import org.springframework.fu.kofu.webflux.webFlux
 import org.springframework.web.reactive.function.server.ServerResponse.ok
@@ -12,24 +11,23 @@ import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
-import org.springframework.http.codec.json.Jackson2JsonDecoder
-import org.springframework.http.codec.json.Jackson2JsonEncoder
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.buildAndAwait
 import reactor.netty.http.client.HttpClient
 import com.fasterxml.jackson.module.kotlin.*
+import org.springframework.http.codec.json.Jackson2JsonDecoder
 
 val httpClient = HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE)
 val objectMapper = jacksonObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-val webClient = WebClient.builder().clientConnector(ReactorClientHttpConnector(httpClient))
-	.codecs { configurer ->
-		configurer.defaultCodecs().jackson2JsonEncoder(Jackson2JsonEncoder(objectMapper, MediaType.APPLICATION_JSON))
-		configurer.defaultCodecs().jackson2JsonDecoder(Jackson2JsonDecoder(objectMapper, MediaType.APPLICATION_JSON))
-	}
+val jackson2JsonDecoder = Jackson2JsonDecoder(objectMapper, MediaType.APPLICATION_JSON)
+val webClient = WebClient.builder()
+	.clientConnector(ReactorClientHttpConnector(httpClient))
+	.codecs{ it.defaultCodecs().jackson2JsonDecoder(jackson2JsonDecoder) }
 	.build()
 
 suspend fun getLatestRelease(request: ServerRequest) = run {
-	val releases = webClient.get().uri("https://api.github.com/repos/jetbrains/kotlin/tags").retrieve().awaitBody<List<Release>>()
+	val url = "https://api.github.com/repos/jetbrains/kotlin/tags"
+	val releases = webClient.get().uri(url).retrieve().awaitBody<List<Release>>()
 
 	releases.filterNot{it.name.contains("-")}.firstOrNull()?.name?.let { name ->
 		ok().bodyValueAndAwait(name)
