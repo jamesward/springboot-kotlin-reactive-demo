696450eb2b08aad12cf523534bd9b9577db61bdb
James Ward <james@jamesward.com>
2021-06-14 11:22:39 -0600

    non-blocking

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.3"
+	id("org.springframework.boot") version "2.5.1"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.4.31"
-	kotlin("plugin.spring") version "1.4.31"
+	kotlin("jvm") version "1.5.10"
+	kotlin("plugin.spring") version "1.5.10"
 }
 
 java {
--- src/main/kotlin/DemoApplication.kt
+++ src/main/kotlin/DemoApplication.kt
@@ -1,34 +1,27 @@
 package demo
 
-import io.netty.resolver.DefaultAddressResolverGroup
+import kotlinx.coroutines.delay
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.awaitBody
-import org.springframework.http.client.reactive.ReactorClientHttpConnector
-import reactor.netty.http.client.HttpClient
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
 
 
+@ExperimentalTime
 @SpringBootApplication
 @RestController
 class DemoApplication {
-	val httpClient = HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE)
-	val webClient = WebClient.builder().clientConnector(ReactorClientHttpConnector(httpClient)).build()
-
-	@GetMapping("/")
-	suspend fun index() = run {
-		val url = "https://api.github.com/repos/jetbrains/kotlin/tags"
-		val releases = webClient.get().uri(url).retrieve().awaitBody<List<Release>>()
-
-		releases.filterNot{it.name.contains("-")}.firstOrNull()?.name ?: "not found"
-	}
+    @GetMapping("/")
+    suspend fun index() = run {
+        delay(Duration.seconds(5))
+        "hello, world"
+    }
 
 }
 
-data class Release(val name: String)
-
+@ExperimentalTime
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+    runApplication<DemoApplication>(*args)
 }
