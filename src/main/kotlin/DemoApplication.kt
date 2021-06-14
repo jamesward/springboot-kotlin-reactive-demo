9aa670da37bd8dc06fbee7e924834fdeae2661b9
James Ward <james@jamesward.com>
2021-06-14 11:24:10 -0600

    blocking

--- src/main/kotlin/DemoApplication.kt
+++ src/main/kotlin/DemoApplication.kt
@@ -1,27 +1,21 @@
 package demo
 
-import kotlinx.coroutines.delay
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import kotlin.time.Duration
-import kotlin.time.ExperimentalTime
 
 
-@ExperimentalTime
 @SpringBootApplication
 @RestController
 class DemoApplication {
     @GetMapping("/")
-    suspend fun index() = run {
-        delay(Duration.seconds(5))
+    fun index() = run {
+        Thread.sleep(5000)
         "hello, world"
     }
-
 }
 
-@ExperimentalTime
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
 }
