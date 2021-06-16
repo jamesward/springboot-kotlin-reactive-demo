49e3378d7cdf76f207d1e4e3dcbbd1987831882a
James Ward <james@jamesward.com>
2021-06-16 16:22:18 -0600

    nosleep

--- src/main/kotlin/DemoApplication.kt
+++ src/main/kotlin/DemoApplication.kt
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController
 class DemoApplication {
     @GetMapping("/")
     fun index() = run {
-        Thread.sleep(5000)
         "hello, world"
     }
 }
